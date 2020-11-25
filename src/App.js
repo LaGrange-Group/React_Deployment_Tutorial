47b7d4d2d8a11483a9c292b686f39a9c84ceb567
David <davidnlagrange@hotmail.com>
2020-11-25 17:16:55 -0600

    Made Change on App.js Regarding Message for Page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Here is a new change to our project!</p>
+        <p>Dysprosium Rocks!</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
